f0658831620b133d62c160f917b7ca905f2c76cd
renji123 <revathyrenjit@gmail.com>
2023-10-19 20:00:21 +0530

    docker file added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <div>Ren shop</div>
+    <h4>Hello World!</h4>
+    <h2>I am Renjith</h2>
+    <h3>this is the third achange</h3>
+    </>
+  )
 }
 
-export default App;
+export default App
+
+
+
